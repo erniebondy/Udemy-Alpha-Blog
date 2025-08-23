99ed58f7c3eb25b4c7b5ea4d10c2ffef81f9dd60
erniebondy <erniebondy@hotmail.com>
2025-08-23 13:02:44 -0700

    Add edit/update/delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,62 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    ## This can be used for debugging purposes
+    ##render plain: params[:article]
+
+    ## Using 'require' and 'permit' are security features
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      #render plain: @article.inspect
+
+      ## notice OR alert
+      flash[:notice] = "Article was created successfully!"
+
+      ## Redirect to the newly created article page
+
+      ## Short-hand
+      ## redirect_to articles_path @article
+      ## redirect_to articles_path #article_path(@article)
+      
+      redirect_to articles_path
+    else
+      ## Article save failed, re-render the 'new' page
+      render "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+    # if @article.save
+    #   flash[:notice] = "Article was created successfully!"
+    #   redirect_to articles_path
+    # else
+    #   render "edit"
+    # end
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully!"
+      #redirect_to @article
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   
   ## The routes are related to the URL
   ## resources :articles -> create all routes for 'articles'
+  ## This is a short hand
+  ## REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete)
+  ## to CRUD actions
+  resources :articles
   ## Use 'only:' to limit the generated routes
-  resources :articles, only: [:show, :index]
+  ##resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   
 end
