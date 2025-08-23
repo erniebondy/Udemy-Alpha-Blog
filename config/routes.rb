639975cdf08e96443711fba72ca79dacbdb91fa1
erniebondy <erniebondy@hotmail.com>
2025-08-22 18:11:55 -0700

    Add Article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   ## The routes are related to the URL
   ## resources :articles -> create all routes for 'articles'
   ## Use 'only:' to limit the generated routes
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   
 end
