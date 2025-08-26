d79b7bf1c3cfd5c0a4938f644a67d5a29c404d2e
erniebondy <erniebondy@hotmail.com>
2025-08-25 17:03:09 -0700

    Add Bootstrap stying

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_22_204403) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_25_212442) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 end
