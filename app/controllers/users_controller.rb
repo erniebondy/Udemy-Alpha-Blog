a3ce85835e47f366df4b4df5bebc2ef128138c0c
erniebondy <erniebondy@hotmail.com>
2025-08-29 14:12:52 -0700

    Add authenticate users helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -8,6 +10,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      ## Also sign in user
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{ @user.username }, to the Alpha Blog!"
       redirect_to articles_path
     else
@@ -16,16 +20,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:notice] = "Your account info was updated!"
@@ -43,6 +47,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user= User.find(params[:id])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,21 @@ module ApplicationHelper
     image_tag("https://www.gravatar.com/avatar/#{hash}?s=#{size}", alt: user.username, class: "rounded shadow mx-auto d-block")
     
   end
+
+  def current_user
+    # if session[:user_id]
+    #   User.find(session[:user_id])
+    # end
+
+    ## ||= operator checks if '@current_user' is present
+    ## if not, the right hand statement is executed
+    ## and also assigns the value to the variable
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    ## !! converts to boolean and returns (in this case)
+    !!current_user
+  end
+
 end
