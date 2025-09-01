7b011c8f6c24e5b383b4c803320e1eb561ca0567
erniebondy <erniebondy@hotmail.com>
2025-09-01 14:34:06 -0700

    before SCSS migration

---
+++ test/models/category_test.rb
@@ -0,0 +1,11 @@
+## This is needed for all of our test files
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+
+  test "category should be valid" do
+    @category = Category.new(name: "Sports")
+    assert category.valid?
+  end
+
+end
