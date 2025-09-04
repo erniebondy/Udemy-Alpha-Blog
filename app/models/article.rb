6140822b3819fc81dababf990729ec4824ec53e2
erniebondy <erniebondy@hotmail.com>
2025-09-03 21:23:04 -0700

    Add article_category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@
 class Article < ApplicationRecord
   ## Article only has 1 associated user
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   ## Active Record validation
   ## 'presence: true' - Forces the data to be NOT NULL
   ## 'length: {...}'  - Forces min/max string length
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_01_222041) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_04_034237) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
