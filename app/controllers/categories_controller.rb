c3ae1de0a602d4e5864a10dc557f509a11fa616d
erniebondy <erniebondy@hotmail.com>
2025-09-04 01:27:44 -0700

    Add edit category. Add edit button to categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def new
     @category = Category.new
@@ -21,10 +22,26 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 3)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category was updated!"
+      redirect_to categories_path
+    else
+      render "edit"
+    end
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   ## This ensures that the params object contains a key called "category".
   ## If it doesn’t, Rails will raise an ActionController::ParameterMissing error.
